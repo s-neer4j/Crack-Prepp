ee147455cec4a0a4e24c50135bb6a2189819e6ab
Neeraj Singh <145915670+s-neer4j@users.noreply.github.com>
2024-09-29 18:20:54 +0530

    Update Check_reverse_number.py

--- Check_reverse_number.py
+++ Check_reverse_number.py
@@ -1,3 +1,18 @@
+        
 def rev_num(num):
-  rev_number = 0
-  whi
+  return int(str(num)[::-1])
+while True:
+  number = input("Enter a five-digit number: ")
+  
+  if len(number) == 5 and number.isdigit():
+    num = int(number)
+    rev_num = rev_num(num)
+    
+    if num == rev_num:
+        print(f"The original number {num} and the reversed number {rev_num} are equal.")
+    else:
+        print(f"The original number {num} and the reversed number {rev_num} are not equal.")
+    break
+else:
+  print("Please enter a valid five-digit number.")       
+  
